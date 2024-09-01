00fd74d8d0723d00a8fed49eb28f1f72fa403d9e
Chaitanya Swaroop <chaitanyaswaroop36@gmail.com>
2024-09-01 12:10:48 +0530

    made all the config functions to get data from appwrite

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -1,14 +1,103 @@
-import { Client, Account } from "appwrite";
+import { Client, Account, Databases, ID, Query } from "appwrite";
 import conf from "../conf/conf";
 
-export class AuthService {
-    client = new Client;
-    account;
+export class DataService {
+  client = new Client();
+  account;
+  databases;
 
-    constructor() {
-        this.client
-            .setEndpoint(conf.appwriteUrl)
-            .setProject(conf.appwriteProjectId)
-        this.account = new Account(this.client)
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+    this.databases = new Databases(this.client);
+  }
+
+  async addNote({
+    Title,
+    Content,
+    Color = "#030712",
+    tags = [],
+    isArchived = false,
+    isTrashed = false,
+  }) {
+    try {
+      const result = await this.databases.createDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        ID.unique(),
+        {
+          Title,
+          Content,
+          Color,
+          tags,
+          isArchived,
+          isTrashed,
+        }
+      );
+      return result;
+    } catch (error) {
+      console.log("Error", error.message);
     }
+  }
+
+  async updateNote(
+    $id,
+    { Title, Content, Color, tags, isArchived, isTrashed }
+  ) {
+    try {
+      const result = await this.databases.updateDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        $id,
+        { Title, Content, Color, tags, isArchived, isTrashed }
+      );
+      return result;
+    } catch (error) {
+      console.log("Error", error.message);
+    }
+  }
+
+  async deleteNote($id) {
+    try {
+      const result = await this.databases.deleteDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        $id
+      );
+      return result;
+    } catch (error) {
+      console.log("Error", error.message);
+    }
+  }
+
+  async getNote($id) {
+    try {
+      const result = await this.databases.getDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        $id
+      );
+      return result;
+    } catch (error) {
+      console.log("Error:", error.message);
+    }
+  }
+
+  async getAllNotes() {
+    try {
+      const response = await this.databases.listDocuments(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId
+      );
+      return response.documents;
+    } catch (error) {
+      console.log("Error:", error.message);
+    }
+  }
 }
+
+const dataService = new DataService();
+
+export default dataService;
